a7a0964865d045bb5a90628a5724818c2b1b7922
Hyhang <hyhang_rsw@163.com>
2019-12-11 14:46:28 +0800

    FIX：修改路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,18 +6,43 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login')
   },
   {
     path: '/laboratory',
     redirect: '/laboratory/workspace',
-    component: () => import(/* webpackChunkName: "laboratory" */ '../views/Layout.vue'),
+    component: () => import(/* webpackChunkName: "laboratory" */ '../views/Layout'),
     children: [
       {
         path: 'workspace',
         name: 'workspace',
         component: () => import(/* webpackChunkName: "workspace" */ '../views/WorkSpace')
-      }
+      },
+      {
+        path: 'analyse',
+        name: 'analyse',
+        component: () => import(/* webpackChunkName: "analyse" */ '../views/WorkSpace')
+      },
+      {
+        path: 'mock1',
+        name: 'mock1',
+        component: () => import(/* webpackChunkName: "mock1" */ '../views/Mock1')
+      },
+      {
+        path: 'mock2',
+        name: 'mock2',
+        component: () => import(/* webpackChunkName: "mock2" */ '../views/WorkSpace')
+      },
+      {
+        path: 'mock3',
+        name: 'mock3',
+        component: () => import(/* webpackChunkName: "mock3" */ '../views/WorkSpace')
+      },
+      {
+        path: 'setting',
+        name: 'setting',
+        component: () => import(/* webpackChunkName: "setting" */ '../views/WorkSpace')
+      },
     ]
   }
 ]
