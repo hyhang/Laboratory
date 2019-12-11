b068dd65f92103e8312c14af9ffa9605092e3741
Hyhang <hyhang_rsw@163.com>
2019-12-11 14:45:12 +0800

    ADD：添加下拉配置项

---
+++ src/assets/Options.js
@@ -0,0 +1,21 @@
+export const LEVEL = {
+  goods: {
+    label: '货品维度',
+    value: 0
+  },
+  goodsAndStock: {
+    label: '货品+仓维度',
+    value: 1
+  }
+}
+
+export const CONFIRM = {
+  yes: {
+    label: '是',
+    value: 1
+  },
+  no: {
+    label: '否',
+    value: 0
+  }
+}
