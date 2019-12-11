bda83d2cd30931144ee0c91f9683c172c5ba9356
Hyhang <hyhang_rsw@163.com>
2019-12-11 14:44:49 +0800

    ADD：配置代理服务器

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:7001/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
