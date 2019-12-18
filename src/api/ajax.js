970352dcef9a0a83f5027e7a886cb8a45686edcc
Hyhang <hyhang_rsw@163.com>
2019-12-18 17:24:04 +0800

    ADD：请求拦截器新增对需要token凭证的接口请求头添加token

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,13 +1,24 @@
 import axios from 'axios'
 import qs from 'qs'
-
+import { getItem } from '@/utils/sessionStore'
 axios.defaults.timeout = 20000
 
 axios.interceptors.request.use( config => {
-  const { method, data } = config
+  const { method, data, headers } = config
+  const { needToken } = headers
+  const userInfo = getItem('userInfo')
   if (method.toUpperCase() === 'POST' && data && typeof data === 'object') {
     //qs将｛｝转化为urlencoded编码的字符串
     config.data = qs.stringify(data)
+  }
+  if (needToken) {
+    const token = userInfo.token
+    if (token) {
+      headers.Authorization = token
+    } else {
+      const error = new Error('没有授权的身份！')
+      throw error
+    }
   }
   
   return config
